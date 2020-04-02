7cae94922fb276507110664ca91bc9d022efa79b
Travis Gerke <tgerke@hsph.harvard.edu>
2020-04-02 14:46:59 -0400

    ingest data

---
+++ R/import.R
@@ -0,0 +1,10 @@
+googlesheets4::sheets_deauth()
+
+dat <- googlesheets4::read_sheet("1zu9qEWI8PsOI_i8nI_S29HDGHlIp2lfVMsGxpQ5tvAQ",
+                                 n_max = 50, na = "0")
+
+readr::write_rds(dat,
+        here::here("data-snapshots", 
+                   glue::glue("policy_", 
+                              format(Sys.time(), "%Y-%m-%d_%H:%MEST"),
+                              ".rds")))
